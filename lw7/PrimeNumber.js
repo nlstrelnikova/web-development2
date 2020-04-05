be9d144f909bcc3cb4ba35a3ee57a6481d90b3a6
CIBERWORLD <nereyda@mail.ru>
2020-04-05 12:40:47 +0300

    space

--- lw7/PrimeNumber.js
+++ lw7/PrimeNumber.js
@@ -1,38 +1,38 @@
 function isPrimeNumber(n){
-  let arr = n
-  let isPrime
+  let arr = n;
+  let isPrime;
   if ((typeof arr !== "number") && (Object.prototype.toString.call(arr) !== '[object Array]')) {
-    console.log('Переданный параметр не является числом либо массивом')
+    console.log('Переданный параметр не является числом либо массивом');
   } else {
     if (typeof arr === "number") {
       for (let i = 2; i < arr; i++) {
-        isPrime = true
+        isPrime = true;
         if (arr % i == 0) {
-          isPrime = false
-          break
+          isPrime = false;
+          break;
         }
       }
       if (isPrime) {
-        console.log('Результат: ', arr, ' is prime number')
+        console.log('Результат: ', arr, ' is prime number');
       } else {
-        console.log('Результат: ', arr, ' is not prime number')
+        console.log('Результат: ', arr, ' is not prime number');
       }
     } else {
       for(let j=0; j < arr.length; j++) {
         if (typeof arr[j] != "number") { 
-          console.log(arr[j], 'Элемент массива не число')
+          console.log(arr[j], 'Элемент массива не число');
         } else {
           for (let i = 2; i < arr[j]; i++) {
-            isPrime = true
+            isPrime = true;
             if (arr[j] % i == 0) {
-              isPrime = false
-              break
+              isPrime = false;
+              break;
             }
           }
           if (isPrime) {
-            console.log('Результат: ', arr[j], ' is prime number')
+            console.log('Результат: ', arr[j], ' is prime number');
           } else {
-            console.log('Результат: ', arr[j], ' is not prime number')
+            console.log('Результат: ', arr[j], ' is not prime number');
           }
         }
       }
