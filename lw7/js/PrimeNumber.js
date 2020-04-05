2339633e97fbc42eb3fc4bad10412d8a1d43135b
CIBERWORLD <nereyda@mail.ru>
2020-04-05 20:45:27 +0300

    js

---
+++ lw7/js/PrimeNumber.js
@@ -0,0 +1,44 @@
+function isPrimeNumber(n){
+  let arr
+  if (Array.isArray(n)) {
+    arr = n;
+  } else {	
+    arr = [n];
+  }
+  for (let i = 0; i < arr.length; i++) {
+    let digit = arr[i];
+    if (typeof(digit) == "number") {
+      if (isPrimeDigit(digit)) {
+        console.log('Результат: ', digit, ' is prime number');
+      } else {
+        console.log('Результат: ', digit, ' is not prime number');
+      }
+    } else {
+      console.log(digit, 'Переданный параметр не число');		
+    }
+  }
+}
+
+function isPrimeDigit(num) {
+  let isPrime;
+  if (num >= 2)	{
+    isPrime = true;	  
+    for (let i = 2; i < num; i++) {
+      if (num % i == 0) {
+        isPrime = false;
+		break;
+      }
+    }
+  } else {
+    isPrime = false;
+  }
+  return isPrime;  
+}  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
