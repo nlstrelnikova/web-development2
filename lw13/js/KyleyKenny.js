a5da11e2355d9221f0b54f3600ff03f9ec7b73dc
CIBERWORLD <nereyda@mail.ru>
2020-06-20 14:53:26 +0300

    lw13

---
+++ lw13/js/KyleyKenny.js
@@ -0,0 +1,422 @@
+function drawKenny(context){        
+    context.beginPath();
+    context.ellipse(202, 320, 72, 84, Math.PI, 0, Math.PI);
+    context.strokeStyle = '#f76900';
+    context.fillStyle = '#f76900';
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.arc(138, 310, 13, 0, 2 * Math.PI);
+    context.strokeStyle = '#000000';
+    context.fillStyle = '#844921';
+    context.lineWidth = 1;
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.arc(265, 310, 13, 0, 2 * Math.PI);
+    context.strokeStyle = '#000000';
+    context.fillStyle = '#844921';
+    context.lineWidth = 1;
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.arc(254, 306, 5, 0, 2 * Math.PI);
+    context.strokeStyle = '#000000';
+    context.fillStyle = '#844921';
+    context.lineWidth = 1;
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.arc(148, 306, 5, 0, 2 * Math.PI);
+    context.strokeStyle = '#000000';
+    context.fillStyle = '#844921';
+    context.lineWidth = 1;
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle = '#f76900';
+    context.strokeStyle = '#f76900';
+    context.fillRect(153, 320, 97, 20);
+    context.lineWidth = 1;
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle = '#100408';
+    context.strokeStyle = '#100408';
+    context.fillRect(148, 335, 106, 5);
+    context.lineWidth = 1;
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.moveTo(202, 282);
+    context.lineTo(202, 322);
+    context.strokeStyle = '#000000';
+    context.lineWidth = 0.7;
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(252, 290);
+    context.lineTo(254, 302);
+    context.strokeStyle = '#000000';
+    context.lineWidth = 0.7;
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(152, 290);
+    context.lineTo(150, 302);
+    context.strokeStyle = '#000000';
+    context.lineWidth = 0.7;
+    context.stroke();
+
+    context.beginPath();
+    context.arc(200, 200, 80, 0, 2 * Math.PI);
+    context.lineWidth = 2;
+    context.fillStyle = '#f76900';
+    context.strokeStyle = '#E65A00';
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.ellipse(200, 206, 52, 58, Math.PI / 2, 0, 2 * Math.PI);
+    context.strokeStyle = '#000000';
+    context.lineWidth = 1;
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(199, 245);
+    context.quadraticCurveTo(265, 200, 201, 156);
+    context.moveTo(199, 245);
+    context.quadraticCurveTo(130, 196, 201, 156);
+    context.lineWidth = 2;
+    context.fillStyle = '#FFE0B0';
+    context.strokeStyle = '#FFE0B0';
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.ellipse(183, 198, 22, 19, Math.PI * .62, 0, 2 * Math.PI);
+    context.lineWidth = 0.1;
+    context.fillStyle = '#FFFFFF';
+    context.strokeStyle = '#000000';
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.ellipse(220, 198, 22, 19, Math.PI * .4, 0, 2 * Math.PI);
+    context.lineWidth = 0.1;
+    context.fillStyle = '#FFFFFF';
+    context.strokeStyle = '#000000';
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.arc(189, 195, 2, 0, 2 * Math.PI);
+    context.lineWidth = 2;
+    context.fillStyle = '#000000';
+    context.strokeStyle = '#000000';
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.arc(218, 197, 2, 0, 2 * Math.PI);
+    context.lineWidth = 2;
+    context.fillStyle = '#000000';
+    context.strokeStyle = '#000000';
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.ellipse(200, 200, 46, 52, Math.PI / 2, 0, 3 * Math.PI);
+    context.lineWidth = 1;
+    context.moveTo(201, 156);
+    context.quadraticCurveTo(130, 196, 199, 245);
+    context.moveTo(199, 245);
+    context.quadraticCurveTo(265, 200, 201, 156);
+    context.fillStyle = '#844921';
+    context.strokeStyle = '#844921';
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.lineWidth = 1;
+    context.moveTo(201, 245);
+    context.quadraticCurveTo(198, 270, 203, 275);
+    context.moveTo(201, 245);
+    context.lineTo(190, 270);
+    context.strokeStyle = '#000000';
+    context.stroke();
+	
+    context.beginPath();
+    context.lineWidth = 0.5;
+    context.moveTo(203, 188);
+    context.quadraticCurveTo(201, 190, 202, 200);
+    context.lineTo(202, 200);
+    context.strokeStyle = '#000000';
+    context.stroke();
+}
+
+function drawKyle(context){ 
+    context.beginPath();
+    context.fillStyle = '#218581';
+    context.strokeStyle = '#218581';
+    context.fillRect(415, 325, 93, 20);
+    context.lineWidth = 1;
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle = '#4A454A';
+    context.strokeStyle = '#4A454A';
+    context.moveTo(409, 346);
+    context.arcTo(470, 338, 510, 346, 260);
+    context.lineWidth = 1;
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.ellipse(460, 320, 68, 83, Math.PI, 0, Math.PI);
+    context.arcTo(460, 340, 392, 320, 200);
+    context.strokeStyle = '#FF6118';
+    context.fillStyle = '#FF6118';
+    context.lineWidth = 1;
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.arc(400, 315, 12, 0, 2 * Math.PI);
+    context.strokeStyle = '#000000';
+    context.fillStyle = '#4DC63C';
+    context.lineWidth = 0.5;
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.arc(517, 318, 12, 0, 2 * Math.PI);
+    context.strokeStyle = '#000000';
+    context.fillStyle = '#4DC63C';
+    context.lineWidth = 0.5;
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.arc(410, 315, 5, 0, 2 * Math.PI);
+    context.strokeStyle = '#000000';
+    context.fillStyle = '#4DC63C';
+    context.lineWidth = 0.5;
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.arc(507, 318, 5, 0, 2 * Math.PI);
+    context.strokeStyle = '#000000';
+    context.fillStyle = '#4DC63C';
+    context.lineWidth = 0.5;
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.arc(460, 227, 59, 0, 2 * Math.PI);
+    context.lineWidth = 2;
+    context.fillStyle = '#00A652';
+    context.strokeStyle = '#00A652';
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.arc(460, 210, 70, 0, 2 * Math.PI);
+    context.lineWidth = 2;
+    context.fillStyle = '#FFECC6';
+    context.strokeStyle = '#FFECC6';
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.ellipse(440, 213, 23, 19, Math.PI * .62, 0, 2 * Math.PI);
+    context.lineWidth = 0.3;
+    context.fillStyle = '#FFFFFF';
+    context.strokeStyle = '#FFFFFF';
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.ellipse(480, 213, 23, 19, Math.PI * .3, 0, 2 * Math.PI);
+    context.lineWidth = 0.3;
+    context.fillStyle = '#FFFFFF';
+    context.strokeStyle = '#FFFFFF';
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.arc(449, 216, 1, 0, 2 * Math.PI);
+    context.lineWidth = 2;
+    context.fillStyle = '#000000';
+    context.strokeStyle = '#000000';
+    context.fill();
+    context.stroke();
+	
+    context.beginPath();
+    context.lineWidth = 0.5;
+    context.moveTo(462, 200);
+    context.quadraticCurveTo(460, 205, 460, 210);
+    context.lineTo(460, 212);
+    context.strokeStyle = '#000000';
+    context.stroke();
+
+    context.beginPath();
+    context.arc(471, 216, 1, 0, 2 * Math.PI);
+    context.lineWidth = 2;
+    context.fillStyle = '#000000';
+    context.strokeStyle = '#000000';
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(442, 252);
+    context.lineTo(485, 252);
+    context.lineTo(463, 270);
+    context.lineTo(442, 252);
+    context.lineWidth = 1;
+    context.fillStyle = '#FFFFFF';
+    context.strokeStyle = '#000000';
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(451, 260);
+    context.lineTo(475, 260);
+    context.lineTo(463, 270);
+    context.lineTo(451, 260);
+    context.lineWidth = 1;
+    context.fillStyle = '#000000';
+    context.strokeStyle = '#000000';
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(453, 252);
+    context.lineTo(453, 260);
+    context.lineTo(464, 260);
+    context.lineTo(465, 252);
+    context.lineWidth = 1;
+    context.strokeStyle = '#000000';
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(473, 260);
+    context.lineTo(475, 252);
+    context.lineWidth = 1;
+    context.strokeStyle = '#000000';
+    context.stroke();
+
+    context.beginPath();
+    context.lineWidth = 1;
+    context.moveTo(420, 130);
+    context.lineTo(500, 130);
+    context.arcTo(530, 130, 530, 150,10);
+    context.lineTo(530,190);
+    context.lineTo(390,190);
+    context.lineTo(390,140);
+    context.arcTo(390, 130, 400, 130, 10);
+    context.lineTo(420, 130);
+    context.fillStyle = '#4AC342';
+    context.strokeStyle = '#4AC342';
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.ellipse(388, 212, 35, 13, Math.PI * .62, 0, 2 * Math.PI);
+    context.lineWidth = 0.3;
+    context.fillStyle = '#4AC342';
+    context.strokeStyle = '#000000';
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.ellipse(535, 212, 35, 13, Math.PI * .39, 0, 2 * Math.PI);
+    context.lineWidth = 0.3;
+    context.fillStyle = '#4AC342';
+    context.strokeStyle = '#000000';
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.fillStyle = '#00AD52';
+    context.strokeStyle = '#00AD52';
+    context.moveTo(401, 155);
+    context.lineTo(522, 155);
+    context.lineTo(522, 193);
+    context.arcTo(461, 188, 402, 193, 250);
+    context.lineTo(401, 193);
+    context.lineWidth = 1;
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.strokeStyle = '#000000';
+    context.moveTo(422, 190);
+    context.lineTo(440, 180);
+    context.lineWidth = 2;
+    context.stroke();
+
+    context.beginPath();
+    context.strokeStyle = '#000000';
+    context.moveTo(500, 190);
+    context.lineTo(480, 180);
+    context.lineWidth = 2;
+    context.stroke();
+
+    context.beginPath();
+    context.strokeStyle = '#000000';
+    context.moveTo(462, 285);
+    context.lineTo(462, 333);
+    context.lineWidth = 1;
+    context.stroke();
+
+    context.beginPath();
+    context.strokeStyle = '#000000';
+    context.moveTo(430, 293);
+    context.lineTo(430, 315);
+    context.lineTo(450, 315);
+    context.lineTo(450, 293);
+    context.lineTo(430, 293);
+    context.lineTo(440, 305);
+    context.lineTo(450, 293);
+    context.lineWidth = 1;
+    context.stroke();
+
+    context.beginPath();
+    context.strokeStyle = '#000000';
+    context.moveTo(475, 293);
+    context.lineTo(475, 315);
+    context.lineTo(495, 315);
+    context.lineTo(495, 293);
+    context.lineTo(475, 293);
+    context.lineTo(485, 305);
+    context.lineTo(495, 293);
+    context.lineWidth = 1;
+    context.stroke();
+
+    context.beginPath();
+    context.strokeStyle = '#000000';
+    context.moveTo(504, 293);
+    context.lineTo(506, 313);
+    context.lineWidth = 0.7;
+    context.stroke();
+
+    context.beginPath();
+    context.strokeStyle = '#000000';
+    context.moveTo(416, 293);
+    context.lineTo(412, 311);
+    context.lineWidth = 0.7;
+    context.stroke();
+    context.closePath();
+}		
+
+window.onload = start;
